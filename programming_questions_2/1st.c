3000cfee050d7c8ed032d0735d4a6a82d96c0b2e
JoelMathewV <joelmathewv0819@gmail.com>
2021-06-09 17:54:00 +0530

    proram to convert given KM to m and cm

---
+++ programming_questions_2/1st.c
@@ -0,0 +1,13 @@
+//1. Write a C program to convert given KM to meter and centimeter
+#include<stdio.h>
+int main(){
+    int n;
+    int m;
+    int cm;
+    printf("input the distance: ");
+    scanf("%d", &n);
+    m = n*1000;
+    cm = m*100;
+    printf("the distance in m is %d and in cm is %d", m, cm);
+    return 0;
+}

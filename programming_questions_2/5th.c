57e81adaa272e294724fb13021dc0f6ae1901838
JoelMathewV <joelmathewv0819@gmail.com>
2021-06-10 00:25:25 +0530

    tweaks to prblm 5th and 6th

--- programming_questions_2/5th.c
+++ programming_questions_2/5th.c
@@ -3,7 +3,8 @@
 int main(){
     int n;
     int ans;
+    printf("enter the no. ");
     scanf("%d", &n);
-    (n%2==0)?(printf("even number")):(printf("odd number"));
+    (n%2==0)?(printf("it is a even number")):(printf("it is a odd number"));
     return 0;
 }

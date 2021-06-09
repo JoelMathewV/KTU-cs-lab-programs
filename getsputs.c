539b4275f09b494efedab144a604964679e3943c
JoelMathewV <joelmathewv0819@gmail.com>
2021-06-09 21:58:30 +0530

    gets puts example

---
+++ getsputs.c
@@ -0,0 +1,10 @@
+//2. Write a program to accept your name, roll number branch and percentage and print it
+
+#include<stdio.h>
+int main(){
+    char name[20];
+    gets(name);
+    printf("the string is ");
+    puts(name);
+    return 0;
+}

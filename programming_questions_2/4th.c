6665b4568329cefaf73f14402d931d3a60e6b945
JoelMathewV <joelmathewv0819@gmail.com>
2021-06-09 23:59:10 +0530

    tweaks to 4th problem

--- programming_questions_2/4th.c
+++ programming_questions_2/4th.c
@@ -1,14 +1,12 @@
 //4. Write a program to read a six digit number and find the sum of first and last digits of the number
 #include<stdio.h>
 int main(){
-    int n;
-    int a;
-    int b;
-    int c;
+    int n,a,b,c;
+    printf("enter a six digit no. ");
     scanf("%d",&n);
     a = n%10;
     b = n/100000;
     c = a+b;
-    printf("%d",c);
+    printf("the sum is %d",c);
     return 0;
 }

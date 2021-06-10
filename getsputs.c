b5c45123ad15307308c1dfbcab1bf5d2ed46ea93
JoelMathewV <joelmathewv0819@gmail.com>
2021-06-10 14:36:40 +0530

    small tweak to 2nd program to use gets() effeciently

--- getsputs.c
+++
@@ -1,9 +0,0 @@
-// An example to see how to use gets() and puts() functions
-#include<stdio.h>
-int main(){
-    char name[20];
-    gets(name);
-    printf("the string is ");
-    puts(name);
-    return 0;
-}

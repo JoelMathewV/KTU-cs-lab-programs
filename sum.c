9f7941eafbda013e4d394d0cf78e1674fc375d51
JoelMathewV <joelmathewv0819@gmail.com>
2021-06-15 13:01:06 +0530

    small tweak

--- sum.c
+++ sum.c
@@ -3,7 +3,7 @@ int main(){
     int i,n,sum=0;
     scanf("%d",&n);
     for(i = 1;i<=n;i++){
-        sum=sum+1;
+        sum=sum+i;
     }
     printf("the first %d natural numbers is %d",n,sum);
     return 0;

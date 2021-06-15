e4e8654912afdf3531ffb80971fde33e0e335053
JoelMathewV <joelmathewv0819@gmail.com>
2021-06-15 15:06:37 +0530

    new 2 problems of today

---
+++ problems_15_june/Nnosum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int i,n,sum=0,a;
+    scanf("%d",&n);
+    for(i = 1;i<=n;i++){
+        printf("enter the %d no",i);
+        scanf("%d",&a);
+        sum=sum+a;
+    }
+    printf("the total sum is %d",sum);
+    return 0;
+}

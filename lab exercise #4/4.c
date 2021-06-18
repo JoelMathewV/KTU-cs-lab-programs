66bba10585710fe1ea9a3bfd13ff9b361f108af7
JoelMathewV <joelmathewv0819@gmail.com>
2021-06-18 17:46:45 +0530

    perfect no problem and minute other changes

--- lab exercise #4/4.c
+++ lab exercise #4/4.c
@@ -1,12 +1,21 @@
 //4. Write a program to check whether a number is perfect or not.(eg 6=1+2+3)
 #include<stdio.h>
 int main(){
-    int n,i=1;
+    int n,sum=0,i;
     printf("enter the no ");
     scanf("%d",&n);
-    while (i<=10){
-        printf("%d x %d = %d \n",n,i,n*i);
-        i++;
+    printf("the factors are: ");
+    for(i=1;i<n;i++){
+        if(n%i==0){
+            sum+=i;
+            printf("%d ",i);
+        }
+    }
+    if(sum==n){
+        printf("\nperfect number");
+    }
+    else{
+        printf("\nnot a perfect number");
     }
     return 0;
 }

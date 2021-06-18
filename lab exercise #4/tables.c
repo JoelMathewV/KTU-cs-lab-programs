2fb5960af0456d76fe9edef4af4f2d55a5b17d80
JoelMathewV <joelmathewv0819@gmail.com>
2021-06-18 15:13:40 +0530

    small change to the first question

--- lab exercise #4/tables.c
+++ lab exercise #4/tables.c
@@ -1,11 +1,12 @@
 //Print the multiplication table of a given number. ( use while)
 #include<stdio.h>
 int main(){
-    int n,i;
+    int n,i=1;
     printf("enter the no ");
     scanf("%d",&n);
-    for(i=1;i<=10;i++){
+    while (i<=10){
         printf("%d x %d = %d \n",n,i,n*i);
+        i++;
     }
     return 0;
 }

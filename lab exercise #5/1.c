cefda70cb4ca8cee45bb54880a80db5e4280b1ba
JoelMathewV <joelmathewv0819@gmail.com>
2021-06-22 15:56:23 +0530

    1st problem

--- lab exercise #5/1.c
+++ lab exercise #5/1.c
@@ -1,1 +1,55 @@
 //print the number in words
+#include<stdio.h>
+int main(){
+    int n,r,t,sum = 0;
+    printf("enter the no ");
+    scanf("%d",&n);
+    while(n>0){
+        r=n%10;
+        sum=sum*10+r;
+        n=n/10;
+    }
+    t=sum;
+    while (t>0)
+    {
+        r=t%10;
+        t=t/10;
+        switch (r)
+        {
+        case 0:
+            printf("zero ");
+            break;
+        case 1:
+            printf("one ");
+            break;
+        case 2:
+            printf("two ");
+            break;
+        case 3:
+            printf("three ");
+            break;
+        case 4:
+            printf("four ");
+            break;
+        case 5:
+            printf("five ");
+            break;
+        case 6:
+            printf("six ");
+            break;
+        case 7:
+            printf("seven ");
+            break;
+        case 8:
+            printf("eight ");
+            break;
+        case 9:
+            printf("nine ");
+            break;
+
+        default:
+            break;
+        }
+    }
+    return 0;
+}

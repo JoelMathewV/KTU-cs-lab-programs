3aaf9a3ab3d8a7151baad14976e828b779f79b8f
JoelMathewV <joelmathewv0819@gmail.com>
2021-06-22 19:42:04 +0530

    4th problem

--- lab exercise #5/4.c
+++ lab exercise #5/4.c
@@ -1,1 +1,12 @@
 //find the factorial of a number using for statement
+#include<stdio.h>
+int main(){
+    int n,i;
+    printf("enter the no: ");
+    scanf("%d",&n);
+    for(i=n-1;i>0;i--){
+        n*=i;
+    }
+    printf("the factorial is %d",n);
+    return 0;
+}

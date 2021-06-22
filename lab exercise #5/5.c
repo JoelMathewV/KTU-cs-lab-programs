12aafe481de1c0121b3c262e1ef4234da5e1840d
JoelMathewV <joelmathewv0819@gmail.com>
2021-06-22 20:50:20 +0530

    5th problem

--- lab exercise #5/5.c
+++ lab exercise #5/5.c
@@ -1,1 +1,19 @@
 //check whether the given number is prime or not using for loop
+#include<stdio.h>
+int main(){
+    int n,i,a;
+    printf("enter the no");
+    scanf("%d",&n);
+    for(i=2;i<n;i++){
+        if(n%i==0){
+            a=1;
+        }
+    }
+    if(a==1){
+        printf("not prime number");
+    }
+    else{
+        printf("prime number");
+    }
+    return 0;
+}

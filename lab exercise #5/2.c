3bb323526dd87477ed828eeb5759e6130fc4928b
JoelMathewV <joelmathewv0819@gmail.com>
2021-06-22 15:57:05 +0530

    3rd problem

--- lab exercise #5/2.c
+++ lab exercise #5/2.c
@@ -1,1 +1,21 @@
 //generate the fibonacci series 0 1 1 2 3 5........
+#include<stdio.h>
+int main(){
+    int n,a,b,c,i=1;
+    printf("enter the no");
+    scanf("%d",&n);
+    a=0;
+    b=1;
+    c=1;
+    printf("%d %d ",a,b);
+    do
+    {
+        n--;
+        printf("%d ",c);
+        a=b;
+        b=c;
+        c=a+b;
+    } while (n-2>0);
+    
+    return 0;
+}

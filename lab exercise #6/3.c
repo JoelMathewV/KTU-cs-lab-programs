246557d6de11eee56a01464fadb3a1ffc2247c1d
JoelMathewV <joelmathewv0819@gmail.com>
2021-08-09 23:48:34 +0530

    enter the commit message for your changes. Lines starting

--- lab exercise #6/3.c
+++ lab exercise #6/3.c
@@ -1,1 +1,26 @@
 //3. Write a program to find the second largest number using an array of n numbers.
+#include<stdio.h>
+int main(){
+    int n,max,nxtmax,i;
+    printf("enter the no. of integers");
+    scanf("%d",&n);
+    int arr[n];
+    printf("enter the numbers");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    max=arr[0];
+    nxtmax=arr[0];
+    for(i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    for(i=0;i<n;i++){
+        if(arr[i]>nxtmax && arr[i]<max){
+            nxtmax=arr[i];
+        }
+    }
+    printf("the 2nd largest number is %d",nxtmax);
+    return 0;
+    }

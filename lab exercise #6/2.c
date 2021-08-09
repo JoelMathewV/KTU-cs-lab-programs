6bcb53b5b168cba3e40a1baa09b23521bbd27e6b
JoelMathewV <joelmathewv0819@gmail.com>
2021-08-10 00:01:34 +0530

    2nd problems

--- lab exercise #6/2.c
+++ lab exercise #6/2.c
@@ -1,1 +1,30 @@
 //2. Write a program to interchange the largest and the smallest number in an array.
+#include<stdio.h>
+int main(){
+    int n,max,min,i,maxi,mini,temp;
+    printf("enter the no. of integers");
+    scanf("%d",&n);
+    int arr[n];
+    printf("enter the numbers");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    max=arr[0];
+    min=arr[0];
+    for(i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+            maxi=i;
+        }
+        if(arr[i]<min){
+            min=arr[i];
+            mini=i;
+        }
+    }
+    arr[mini]=max;
+    arr[maxi]=min;
+    for(i=0;i<n;i++){
+        printf("%d\t",arr[i]);
+    }
+return 0;
+}

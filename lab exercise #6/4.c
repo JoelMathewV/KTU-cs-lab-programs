f18b70f14eaa68bc052fccebcd16ffe8f2f002f6
JoelMathewV <joelmathewv0819@gmail.com>
2021-08-10 11:15:50 +0530

    test

--- lab exercise #6/4.c
+++ lab exercise #6/4.c
@@ -1,1 +1,19 @@
 //4. Write a program to insert a number at a given location in an array.
+#include<stdio.h>
+int main(){
+    int n,loc,m,i;
+    printf("enter the no. of integers");
+    scanf("%d",&n);
+    int arr[n];
+    printf("enter the numbers");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("enter the number and the location");
+    scanf("%d %d",&m,&loc);
+    arr[loc]=m;
+    for(i=0;i<n;i++){
+        printf("%d\t",arr[i]);
+    }
+    return 0;
+}

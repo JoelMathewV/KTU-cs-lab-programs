d82c47cf406a4ace0427c813276e05b32db26c6e
JoelMathewV <joelmathewv0819@gmail.com>
2021-08-14 08:31:48 +0530

    1st problem improved

--- lab exercise #7/1.c
+++ lab exercise #7/1.c
@@ -4,26 +4,32 @@ int main(){
     int r,c,i,j;
     printf("enter the no of rows and columns");
     scanf("%d %d",&r,&c);
-    int mata[r][c];
-    int matb[r][c];
-    int result[r][c];
+    int mata[r][c],matb[r][c],result[r][c];
+
+    //first matrix
     printf("enter the numbers of 1st matrix");
     for (int i = 0; i < r; i++){
         for (int j = 0; j < c; j++){
             scanf("%d",&mata[i][j]);
         }
     }
+
+    //second matrix
     printf("enter the numbers of 2nd matrix");
     for (int i = 0; i < r; i++){
         for (int j = 0; j < c; j++){
             scanf("%d",&matb[i][j]);
         }
     }
+
+    //calculation
     for (int i = 0; i < r; i++){
         for (int j=0;j<c;j++){
             result[i][j]=mata[i][j]+matb[i][j];
         }
     }
+
+    //output
     printf("the sum of the matrix is \n");
     for (int i = 0; i < r; i++){
         for (int j=0;j<c;j++){

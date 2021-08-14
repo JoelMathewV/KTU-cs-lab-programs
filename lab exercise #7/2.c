6d4e7edbaad85b0b79d6c18cbe1ae5b95e6d0adb
JoelMathewV <joelmathewv0819@gmail.com>
2021-08-14 08:47:20 +0530

    2nd problem matrix multiplication

--- lab exercise #7/2.c
+++ lab exercise #7/2.c
@@ -1,1 +1,43 @@
 //2. Write a Program to multiply two Matrices.
+#include<stdio.h>
+int main(){
+    int r,c,i,j,k;
+    printf("enter the no of rows and columns");
+    scanf("%d %d",&r,&c);
+    int mata[r][c],matb[r][c],result[r][c];
+    printf("enter the numbers of 1st matrix");
+    for (int i = 0; i < r; i++){
+        for (int j = 0; j < c; j++){
+            scanf("%d",&mata[i][j]);
+        }
+    }
+    printf("enter the numbers of 2nd matrix");
+    for (int i = 0; i < r; i++){
+        for (int j = 0; j < c; j++){
+            scanf("%d",&matb[i][j]);
+        }
+    }
+
+    //calculation
+    for (int i=0;i<r;i++){
+        for (int j = 0; j < c; j++)
+        {   result[i][j]=0;
+            for (int k = 0; k < c; k++)
+            {
+                result[i][j]+= mata[i][k]*matb[k][j];
+            }
+            
+        }
+        
+    }
+
+    //printing result
+    printf("the ans is \n");
+    for (int i = 0; i < r; i++){
+        for (int j=0;j<c;j++){
+            printf("%d\t",result[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}

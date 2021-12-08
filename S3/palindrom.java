88f32fae065ff19acaeaed800ce79ae5ab250ea3
JoelMathewV <joelmathewv0819@gmail.com>
2021-12-09 01:42:26 +0530

    palindrom

--- S3/palindrom.java
+++ S3/palindrom.java
@@ -1,8 +1,23 @@
 package S3;
+import java.util.Scanner;
+
 //import java.io.*;
 //import java.util.*;
 class Palindrom{
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int flag = 0;
+        String s = sc.nextLine();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == s.charAt(s.length()-1-i)){
+                flag = 1;
+            }
+        }
+        if(flag==0){
+            System.out.println("not a palindrom");
+        }
+        else{
+            System.out.println("it is a palindrom");
+        }
     }
 }

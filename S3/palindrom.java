6a25d26fa7be08e07ffa80aeca0d1c53da95578f
JoelMathewV <joelmathewv0819@gmail.com>
2021-12-09 00:35:30 +0530

    stack and palindrom half

---
+++ S3/palindrom.java
@@ -0,0 +1,8 @@
+package S3;
+//import java.io.*;
+//import java.util.*;
+class Palindrom{
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ S3/stack.c
@@ -0,0 +1,9 @@
+//incomplete - work still in progress
+#include<stdio.h>
+int main(){
+    int n;
+    while(n>1){
+
+    }
+    return 0;
+}

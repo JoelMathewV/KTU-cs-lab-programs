904ec1881494f0c28d69cdbb92ef439b15df0c8f
JoelMathewV <joelmathewv0819@gmail.com>
2021-12-09 12:50:41 +0530

    more organised

--- S3/OOPs/Palindrom.java
+++ S3/OOPs/Palindrom.java
@@ -1,8 +1,6 @@
-package S3;
-import java.util.Scanner;
-
-//import java.io.*;
-//import java.util.*;
+//package S3.OOPs;
+import java.io.*;
+import java.util.*;
 class Palindrom{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);

3be782b6845f73cdd6f5e0d998ae9974f68c3e8b
JoelMathewV <joelmathewv0819@gmail.com>
2021-12-14 17:09:47 +0530

    stack completed

--- S3/DataStructure/stack.c
+++ S3/DataStructure/stack.c
@@ -1,47 +1,51 @@
-//incomplete - work still in progress
 #include<stdio.h>
-int arr[10], top=0,element ;
+int arr[10], top=-1,element ;
 void push(){
     if(top+1<10){
-        arr[top] = element;
+        arr[top+1] = element;
         top+=1;
     }
     else{
-        printf("overflow");
+        printf("overflow\n");
     }
 }
-void pop(int arr[10], int top, int element){
+void pop(){
     if(top>=0){
         element = arr[top-1];
+        top--;
+    }
+    else{
+        printf("stack underflow\n");
     }
 }
 
-void display(int arr[10]){
+void display(){
     int i;
-    for(i=0;i<10;i++){
+    for(i=0;i<=top;i++){
         printf("%d ",arr[i]);
     }
+    printf("\n");
 }
 int main(){
     int n=2;
     int a;
     while(n>1){
-        printf("choose from following options\n 1.push\n 2.pop\n 3.display\n 4.exit");
+        printf("choose from following options\n 1.push\n 2.pop\n 3.display\n 4.exit\n");
         scanf("%d",&a);
         switch (a)
         {
         case 1:
-            printf("enter the element to push");
+            printf("enter the element to push\n");
             scanf("%d",&element);
             push();
             break;
 
         case 2:
-            pop(arr, top, element);
+            pop();
             break;
 
         case 3:
-            display(arr);
+            display();
             break;
 
         case 4:

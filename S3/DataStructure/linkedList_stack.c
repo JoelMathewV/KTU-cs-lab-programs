f7a07af533c23637d09c6e73376ea5800b114d34
Joel Mathew V <joelmathewv0819@gmail.com>
2022-01-31 15:20:09 +0530

    stack operations using linked list

--- S3/DataStructure/linkedList_stack.c
+++ S3/DataStructure/linkedList_stack.c
@@ -4,36 +4,6 @@ struct node{
     int data;
     struct node *link;
 };
-void push();
-void pop();
-void display();
-struct node *header;
-int main(){
-int op;
-char ch;
-while (1)
-{
-    printf("1.PUSH\n2.POP\n3.DISPLAY");
-    scanf("%d",&op);
-    switch (op)
-    {
-        case 1 : push();
-                 break;
-        case 2 : pop();
-                 break;
-        case 3 : display();
-                 break; 
-        default: printf("Error");
-                 break;
-    }
-	printf("\nDo you want to continue y/n ");
-	scanf(" %ch",&ch);
-	if(ch == 'n'){
-		break;
-	}
-}
-return 0;
-}
 void push(){
     int data;
     struct node *temp;
@@ -48,7 +18,6 @@ void push(){
         temp->link=header;
         header=temp;
     }
-
 }
 void pop(){
     if(header==NULL){
@@ -71,8 +40,33 @@ void display(){
         {
             printf("%d\t",temp->data);
             temp = temp->link;
-        }
-        
-    }
-    
+        }   
+    } 
 }
+struct node *header;
+int main(){
+    int op;
+    char ch;
+    while (1)
+    {
+        printf("1.PUSH\n2.POP\n3.DISPLAY");
+        scanf("%d",&op);
+        switch (op)
+        {
+            case 1 : push();
+                     break;
+            case 2 : pop();
+                     break;
+            case 3 : display();
+                     break; 
+            default: printf("Error");
+                     break;
+        }
+    	printf("\nDo you want to continue y/n ");
+    	scanf(" %ch",&ch);
+    	if(ch == 'n'){
+    		break;
+    	}
+    }
+    return 0;
+    }
